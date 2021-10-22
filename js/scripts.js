fdbd9ccacb97d385925212a066b479bfb8e48f47
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-10-22 10:31:52 -0700

    Add rest of form and first steps in JS

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$("form#form").submit(function(event) {
+  const food=
+  $("input:radio[name=food]:checked").val();
+  const music=
+  $("input:radio[name=food]:checked").val();
+  const animal=
+  $("input:radio[name=food]:checked").val();
+  const color=
+  $("input:radio[name=food]:checked").val();
+  const fun=
+  $("input:radio[name=food]:checked").val();
+  let total=0;
+});
