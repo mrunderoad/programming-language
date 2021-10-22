cc1ee0a87453f29fb13738d1d1dd19f95aa61aa3
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-10-22 12:47:54 -0700

    got js working styled css

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-$(document).ready(function($) { 
+$(document).ready(function() { 
   $("form#form").submit(function(event) {
     event.preventDefault();
+    const name= $("input#name").val();
     const food= $("select#food").val();
     const music= $("select#music").val();
     const animal= $("select#animal").val();
@@ -9,9 +10,25 @@ $(document).ready(function($) {
 
     if (food === 'american' && music === 'rock') {
       $('#html').show();
-    } else if (food === 'asian' && music === 'rock') {
+      $('#css').hide();
+      $('#js').hide();
+    } else if (food === 'american' && music === 'indie') {
+      $('#html').hide();
       $('#css').show();
-
+      $('#js').hide();
+    } else if (food === 'asian' && music === 'rock') {
+      $('#html').hide();
+      $('#css').hide();
+      $('#js').show();
+    } else if (food === 'asian' && music === 'indie') {
+      $('#html').hide();
+      $('#css').show();
+      $('#js').hide();
+    } else if (food === 'american' && animal === 'cat') {
+      $('#html').hide();
+      $('#css').hide();
+      $('#js').show();
+    }
     
   });
 });
